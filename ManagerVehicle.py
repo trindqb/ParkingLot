1e8e62698c67f24f9836fb1957df5ebefa686b1d
trindqb181991 <manhcuong018@gmail.com>
2015-08-27 01:30:09 +0900

    update static metho InitData()

--- ManagerVehicle.py
+++ ManagerVehicle.py
@@ -3,19 +3,25 @@ from Vehicle import *
 
 class ManagerVehicle(object):
     ListVehicle = None
-    Destination = None
+    NumVehicle = None
     def __init__(self,Data):
-        self.Destination = Data[0]
+        self.NumVehicle = Data[0]
         self.ListVehicle = Data[1]
 
     def __getitem__(self, item):
-        if(item == D):
-            return self.Destination
+        if(item == 'NV'):
+            return self.NumVehicle
         elif(item == 'LV'):
             return self.ListVehicle
     def __setitem__(self, key, value):
-        if(key == D):
-            self.Destination = value
+        if(key == 'NV'):
+            self.NumVehicle = value
         elif(key == 'LV'):
             self.ListVehicle = value
+    @staticmethod
+    def InitData(NumVehicle,Type):
+        tmp = []
+        for x in range(NumVehicle):
+            tmp.append(Vehicle(Vehicle.InitVehicle(Type)))
+        return NumVehicle,tmp
 
